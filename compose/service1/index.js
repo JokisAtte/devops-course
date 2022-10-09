5643c66e71b57c63076e69ad47f077725786c0f0
Atte Jokinen <atte.jokinen95@gmail.com>
2022-10-09 21:12:51 +0300

    all working

--- compose/service1/index.js
+++ compose/service1/index.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const request = require("request");
+const axios = require("axios");
 const app = express();
 const port = 8001;
 
-app.all("/", (req, res) => {
-  console.log("Service 1 start");
-  request("compose/service2://service2:9001", function (err, res, body) {
-    if (!err && res.statusCode == 200) {
-      console.log(body);
-    }
-  });
+app.all("/", async (req, res) => {
   const t1 = `Hello from ${req.socket.remoteAddress}:${req.socket.remotePort}`;
   const t2 =
     "Req served at " + req.client.localAddress + ":" + req.client.localPort;
-  console.log(t1);
-  console.log(t2);
-  res.status(200).send();
+  const viesti = await axios.get("http://service2:9001").then((response) => {
+    return response.data;
+  });
+  res.send(t1 + "\n" + t2 + "\n" + viesti);
 });
 
 app.listen(port, () => {
--- compose/service2/index.js
+++ compose/service2/index.js
@@ -3,13 +3,10 @@ const app = express();
 const port = 9001;
 
 app.all("/", (req, res) => {
-  console.log("Service 2 start");
   const t1 = `Hello from ${req.socket.remoteAddress}:${req.socket.remotePort}`;
   const t2 =
     "Req served at " + req.client.localAddress + ":" + req.client.localPort;
-  console.log(t1);
-  console.log(t2);
-  res.status(200).send();
+  res.status(200).send(t1 + "\n" + t2);
 });
 
 app.listen(port, () => {

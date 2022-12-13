59452050ad05ee281172034e79767c533c094fcd
Atte Jokinen <atte.jokinen95@gmail.com>
2022-12-13 22:32:39 +0200

    first test works now

--- api/tests/getState.test.js
+++ api/tests/getState.test.js
@@ -1,12 +1,10 @@
-//import server from "../server.js"
-//import {test, expect} from "jest"
 const server =  require("../server.js");
 const supertest = require("supertest")
-const {test, expect} =  require("jest");
 
 const request = supertest(server)
-
-test("gets text response from the endpoint", async done => {
-    const res = await request.get("/state")
-    expect(res.status).toBe(200) 
+describe("GET /state endpoint tests", () => {
+    it("gets text response from the endpoint", async done => {
+        const res = await request.get("/state")
+        expect(res.status).toBe(200)
+    })
 })

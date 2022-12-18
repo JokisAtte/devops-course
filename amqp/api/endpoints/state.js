c61652400bedcf60eb079b422b9dccae987bc812
Atte Jokinen <atte.jokinen95@gmail.com>
2022-12-18 11:19:58 +0200

    succesfully making calls to UNIX sockets to control the containers

--- amqp/api/endpoints/state.js
+++ amqp/api/endpoints/state.js
@@ -1,14 +1,40 @@
+const request = require('request')
+const http = require('http')
 
 const getState = () => {
     // TODO
-    console.log("get /state Not implemented")
+    console.log('get /state Not implemented')
 }
 
-const putState = () => {
-    // TODO
-    console.log("put /state Not implemented")
+const getOrigContainerId = async () => {
+    const options = {
+        socketPath: '/var/run/docker.sock',
+        path: 'http://localhost/containers/json?ancestor=amqp-orig',
+    }
+
+    return new Promise((resolve, reject) => {
+        const callback = (res) => {
+            res.setEncoding('utf8')
+            res.on('data', (res) => {
+                data = JSON.parse(res)
+                resolve(data[0].Id)
+            })
+            res.on('error', (data) => reject(data))
+        }
+        const clientRequest = http.request(options, callback)
+        clientRequest.end()
+    })
 }
 
+const pauseContainerById = (id) => {}
 
-module.exports = {getState, putState}
-//export default getState
+const putState = async (newState) => {
+    if (newState === 'INIT') {
+    } else if (newState === 'PAUSED') {
+        const id = await getOrigContainerId()
+    } else if (newState === 'SHUTDOWN') {
+    } else if (newState === 'RUNNING') {
+    }
+}
+
+module.exports = { getState, putState }
--- amqp/api/server.js
+++ amqp/api/server.js
@@ -1,37 +1,48 @@
 //const express = require('express');
-const express =  require("express");
-const bodyParser =  require("body-parser");
-const cors =  require("cors");
-const morgan =  require("morgan");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const getMessages =  require("./endpoints/messages.js");
-const runLog =  require("./endpoints/runlog.js");
-const {getState, putState} =  require("./endpoints/state.js");
+const getMessages = require('./endpoints/messages.js')
+const runLog = require('./endpoints/runlog.js')
+const { getState, putState } = require('./endpoints/state.js')
 
-const server = express();
+const server = express()
 
 server.use(cors())
-server.use(morgan('combined'));
-server.use(express.text({type:"*/*"})) //Any Content-Type is populated in request body as text
-let STATE = "INIT"
+server.use(morgan('combined'))
+server.use(express.text({ type: '*/*' })) //Any Content-Type is populated in request body as text
+let STATE = 'INIT'
 
-server.get("/messages", async (req,res) => {
+server.get('/messages', async (req, res) => {
     const messages = await getMessages()
     res.status(200).send(messages)
 })
 
-server.put("/state", (req,res) => {
-    const states = ["INIT", "PAUSED", "RUNNING","SHUTDOWN"]
-    const newState = req.body
-    if(states.includes(newState)){}
-    putState()
+server.put('/state', (req, res) => {
+    const states = ['INIT', 'PAUSED', 'RUNNING', 'SHUTDOWN']
+    const body = req.body
+    const newState = body.replace(/['"]+/g, '') // trim quotemarks from the input
+    console.log('Uus state:', newState)
+    console.log('Vanha state:', STATE)
+    console.log(states.includes(newState))
+    console.log(newState != STATE)
+    if (states.includes(newState) && newState != STATE) {
+        const result = putState(newState)
+        if (result) {
+            STATE = newState
+            res.status(200).send()
+        }
+    }
+    res.status(400).send('bad request or new state is equal to previous state')
 })
 
-server.get("/state", (req,res)=> {
+server.get('/state', (req, res) => {
     res.status(200).send(STATE)
 })
 
-server.get("/run-log", (req,res)=> {
+server.get('/run-log', (req, res) => {
     runLog()
 })
 

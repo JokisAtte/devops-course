52139e6761c136d4e234c4d221ddcc41e9d1f7b2
Atte Jokinen <atte.jokinen95@gmail.com>
2022-12-18 14:24:49 +0200

    fixed bug in fetching the orig container id

--- amqp/api/endpoints/state.js
+++ amqp/api/endpoints/state.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const http = require('http')
+const net = require('net')
 
 const getState = () => {
     // TODO
@@ -17,7 +18,9 @@ const getOrigContainerId = async () => {
             res.setEncoding('utf8')
             res.on('data', (res) => {
                 data = JSON.parse(res)
-                resolve(data[0].Id)
+                orig = data.filter((c) => c.Image === 'amqp-orig')
+                console.log(orig)
+                resolve(orig[0].Id)
             })
             res.on('error', (data) => reject(data))
         }
@@ -26,12 +29,44 @@ const getOrigContainerId = async () => {
     })
 }
 
-const pauseContainerById = (id) => {}
+async function pauseContainer(containerId) {
+    console.log('Pause: ', containerId)
+    const options = {
+        socketPath: '/var/run/docker.sock',
+        path: `http://localhost/containers/${containerId}/pause`,
+    }
+    return new Promise((resolve, reject) => {
+        const callback = (res) => {
+            res.on('data', (data) => {
+                const response = data.toString()
+                console.log('response:', response)
+                if (response.startsWith('HTTP/1.1 204')) {
+                    console.log(`Container ${containerId} paused successfully`)
+                    resolve()
+                } else {
+                    console.error(
+                        `Error pausing container ${containerId}: ${response}`
+                    )
+                    reject(new Error(response))
+                }
+            })
+            res.on('error', (error) => {
+                console.error(
+                    `Error pausing container ${containerId}: ${error}`
+                )
+                reject(error)
+            })
+        }
+        const clientRequest = http.request(options, callback)
+        clientRequest.end()
+    })
+}
 
 const putState = async (newState) => {
     if (newState === 'INIT') {
     } else if (newState === 'PAUSED') {
         const id = await getOrigContainerId()
+        await pauseContainer(id)
     } else if (newState === 'SHUTDOWN') {
     } else if (newState === 'RUNNING') {
     }

4ffb67ae664cfa408ced183b249a961ba3da6956
Atte Jokinen <atte.jokinen95@gmail.com>
2022-12-21 21:48:52 +0200

    Started work on rest of state changes

--- amqp/api/endpoints/state.js
+++ amqp/api/endpoints/state.js
@@ -48,6 +48,10 @@ const getContainerId = async (imageName) => {
     })
 }
 
+const logStateChange = (newState) => {
+    //TODO
+}
+
 // Returns a promise that resolves to a list [boolean, Error | undefined]
 async function pauseContainer(containerId) {
     const options = {
@@ -82,15 +86,64 @@ async function pauseContainer(containerId) {
     })
 }
 
+async function getContainerState(containerId) {
+    return 'null'
+}
+
+// Returns a promise that resolves to a list [boolean, Error | undefined]
+async function setContainerState(containerId, state) {
+    //TODO Pohdi pitäisko olla vaa omat funktiot joka toiminnolle vai mennä samalla?
+    const paths = {
+        PAUSED: `http://localhost/containers/${containerId}/pause`,
+        SHUTDOWN: `http://localhost/containers/${containerId}/stop`,
+        INIT: `http://localhost/containers/${containerId}/start`,
+    }
+    const options = {
+        socketPath: '/var/run/docker.sock',
+        path: `http://localhost/containers/${containerId}/pause`,
+        method: 'POST',
+    }
+    return new Promise((resolve, reject) => {
+        const callback = (res) => {
+            res.on('data', (data) => {
+                const response = data.toString()
+                if (response.startsWith('HTTP/1.1 204')) {
+                    console.log(`Container ${containerId} paused successfully`)
+                    resolve([true, undefined])
+                } else {
+                    console.error(
+                        `Error pausing container ${containerId}: ${response}`
+                    )
+                    reject([false, new Error(response)])
+                }
+            })
+            res.on('error', (error) => {
+                console.error(
+                    `Error pausing container ${containerId}: ${error}`
+                )
+                reject([false, error])
+            })
+        }
+        const clientRequest = http.request(options, callback)
+        clientRequest.end()
+    })
+}
+
 const putState = async (newState) => {
     if (newState === 'INIT') {
+        // TODO Restarttaa kaikki kontit
     } else if (newState === 'PAUSED') {
         const id = await getContainerId('amqp-orig')
         const [paused, error] = await pauseContainer(id)
         error && console.log(error)
         return paused
     } else if (newState === 'SHUTDOWN') {
+        // TODO Kaikille että mene kiinni
     } else if (newState === 'RUNNING') {
+        // TODO origille kutsu että restart
+        const id = await getContainerId('amqp-orig')
+        const [paused, error] = await setContainerState(id, 'RUNNING')
+        error && console.log(error)
     }
     return false
 }

8f9317ab9214bc324f63c409466a94b733e9aadb
Atte Jokinen <atte.jokinen95@gmail.com>
2022-12-23 10:24:15 +0200

    stopping all containers done

--- amqp/api/endpoints/state.js
+++ amqp/api/endpoints/state.js
@@ -90,36 +90,53 @@ async function getContainerState(containerId) {
     return 'null'
 }
 
-// Returns a promise that resolves to a list [boolean, Error | undefined]
-async function setContainerState(containerId, state) {
-    //TODO Pohdi pitäisko olla vaa omat funktiot joka toiminnolle vai mennä samalla?
-    const paths = {
-        PAUSED: `http://localhost/containers/${containerId}/pause`,
-        SHUTDOWN: `http://localhost/containers/${containerId}/stop`,
-        INIT: `http://localhost/containers/${containerId}/start`,
+async function getAllContainers() {
+    let options = {
+        socketPath: '/var/run/docker.sock',
+        path: `http://localhost/containers/json`,
+        method: 'GET',
     }
+    return new Promise((resolve, reject) => {
+        const callback = (res) => {
+            res.on('data', (data) => {
+                const response = JSON.parse(data)
+                console.log(`Get containers done`)
+                resolve([true, response])
+            })
+            res.on('error', (error) => {
+                console.error(`Error getting all containers: ${error}`)
+                reject([false, error])
+            })
+        }
+        const clientRequest = http.request(options, callback)
+        clientRequest.end()
+    })
+}
+
+async function stopOneContainer(containerId) {
     const options = {
         socketPath: '/var/run/docker.sock',
-        path: `http://localhost/containers/${containerId}/pause`,
+        path: `http://localhost/containers/${containerId}/stop`,
         method: 'POST',
     }
     return new Promise((resolve, reject) => {
         const callback = (res) => {
             res.on('data', (data) => {
                 const response = data.toString()
+                console.log('response:', response)
                 if (response.startsWith('HTTP/1.1 204')) {
-                    console.log(`Container ${containerId} paused successfully`)
+                    console.log(`Container ${containerId} stopped successfully`)
                     resolve([true, undefined])
                 } else {
                     console.error(
-                        `Error pausing container ${containerId}: ${response}`
+                        `Error stopping container ${containerId}: ${response}`
                     )
                     reject([false, new Error(response)])
                 }
             })
             res.on('error', (error) => {
                 console.error(
-                    `Error pausing container ${containerId}: ${error}`
+                    `Error stopping container ${containerId}: ${error}`
                 )
                 reject([false, error])
             })
@@ -127,6 +144,28 @@ async function setContainerState(containerId, state) {
         const clientRequest = http.request(options, callback)
         clientRequest.end()
     })
+}
+
+// Returns a promise that resolves to a list [boolean, Error | undefined]
+async function stopAllContainers() {
+    const [resultAC, allContainers] = await getAllContainers()
+    const excludedContainers = [
+        'amqp-api',
+        'gitlab/gitlab-ce:latest',
+        'gitlab/gitlab-runner:alpine',
+    ] //These are image names
+    let allContainersStopped = true
+    resultAC &&
+        allContainers.forEach(async (c) => {
+            if (!excludedContainers.includes(c.Image)) {
+                const [result, error] = await stopOneContainer(c.Id)
+                if (!result) {
+                    allContainersStopped = false
+                    console.log(error)
+                }
+            }
+        })
+    return allContainersStopped
 }
 
 const putState = async (newState) => {
@@ -138,12 +177,12 @@ const putState = async (newState) => {
         error && console.log(error)
         return paused
     } else if (newState === 'SHUTDOWN') {
-        // TODO Kaikille että mene kiinni
+        const stopped = await stopAllContainers()
+        !stopped && console.log('Error with stopping all containers')
+        return stopped
     } else if (newState === 'RUNNING') {
         // TODO origille kutsu että restart
         const id = await getContainerId('amqp-orig')
-        const [paused, error] = await setContainerState(id, 'RUNNING')
-        error && console.log(error)
     }
     return false
 }
